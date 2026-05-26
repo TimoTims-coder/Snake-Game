95d43d7dd57d3fc404465dd974175a61def7ee25
Timothy <adeoluwatomiwa2109@gmail.com>
2026-05-26 14:37:41 +0100

    Initial commit

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function nextTick(){
             drawSnake();
             checkGameOver();
             nextTick();
-        }, 75);
+        }, 150);
     }
     else{
         displayGameOver();
